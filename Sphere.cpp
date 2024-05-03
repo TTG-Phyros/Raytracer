43c7cd4dfbe41d8cab150c043ea2760cd661df0d
Neptune55 <anthony1.girard@epitech.eu>
2024-05-03 15:34:18 +0200

    Include [New Branche]

--- Sphere.cpp
+++ Sphere.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "Sphere.hpp"
+#include "math.h"
 
 RayTracer::Sphere::Sphere(Math::Point3D origin, double radius)
 {
@@ -25,9 +26,9 @@ bool RayTracer::Sphere::hits(RayTracer::Ray &ray)
         ray._origin._z - _origin._z
     );
     Math::Vector3D vector = Math::Vector3D(
-        ray._direction._x - _origin._x,
-        ray._direction._y - _origin._y,
-        ray._direction._z - _origin._z
+        ray._direction._x - ray._origin._x,
+        ray._direction._y - ray._origin._y,
+        ray._direction._z - ray._origin._z
     );
 
     double a = ((vector._x * vector._x) + (vector._y * vector._y) + (vector._z * vector._z));
@@ -36,7 +37,14 @@ bool RayTracer::Sphere::hits(RayTracer::Ray &ray)
 
     double delta = (b * b) - (4 * a * c);
 
-    return delta < 0 ? false : true;
+    if (delta < 0)
+        return false;
+    else {
+        double t1 = (-b + sqrt(delta)) / (2 * a);
+        double t2 = (-b - sqrt(delta)) / (2 * a);
+    }
+
+    return true;
 }
 
 std::ostream &operator<<(std::ostream &cout, RayTracer::Sphere &sphere)
