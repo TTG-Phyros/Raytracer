e8988aa012b9af29c53c7ee426412fb40a7e70a4
Neptune55 <anthony1.girard@epitech.eu>
2024-05-06 17:28:08 +0200

    Include - - Refrato [parsing] - Creating .ppm - Refrato [main]

--- src/main.cpp
+++ src/main.cpp
@@ -24,22 +24,52 @@
 #include "Parser/Parser.hpp"
 #include "Utils/Exception/Exception.hpp"
 
-int create_ppm ()
+int create_header_ppm(std::vector<std::tuple<std::string, std::string>> camerasInfo, std::vector<std::tuple<std::string, std::string>> primitiveInfo)
 {
     std::ofstream image;
     image.open("image.ppm");
 
+    std::string width;
+    std::string height;
+    std::string color_r;
+    std::string color_g;
+    std::string color_b;
+
+    for (const auto& info : camerasInfo) {
+        if (std::get<0>(info) == "width_resolution")
+            width = std::get<1>(info);
+        if (std::get<0>(info) == "height_resolution")
+            height = std::get<1>(info);
+    }
+
+    for (const auto& info : primitiveInfo) {
+        if (std::get<0>(info) == "color_r")
+            color_r = std::get<1>(info);
+        if (std::get<0>(info) == "color_g")
+            color_g = std::get<1>(info);
+        if (std::get<0>(info) == "color_b")
+            color_b = std::get<1>(info);
+    }
+
     if (image.is_open()) {
         image << "p3" << std::endl;
-        image << "1920 1080" << std::endl;
-        image << "255" << std::endl;
+        image << width << " ";
+        image << height << std::endl;
+        image << color_r << " ";
+        image << color_g << " ";
+        image << color_b << std::endl;
 
         for (int y = 0; y < 250; y++) {
             for (int x = 0; x < 250; x++) {
                 image << x << " " << x << " " << x << std::endl;
             }
         }
+        image.close();
+    } else {
+        std::cerr << "Erreur: Impossible d'ouvrir le fichier image.ppm" << std::endl;
+        return -1;
     }
+    return 0;
 }
 
 int main(int ac, char **av)
@@ -49,9 +79,9 @@ int main(int ac, char **av)
             std::cout << "USAGE: ./raytracer <SCENE_FILE>\n\tSCENE_FILE: scene configuration" << std::endl;
         } if (ac == 2) {
             Parser parser(av[1]);
+            create_header_ppm(parser.getCameraInfo(), parser.getPrimitivesInfo());
         } else if ((ac == 3) && (std::string(av[2]) == "--debug")) {
             Parser parser(av[1], "--debug");
-            create_ppm();
         } else {
             throw Exception("Error: invalid arguments");
         }
